2cbfd502e4f1da28f2e79c9cdfa2c69024e45a0e
Mikael Tallbo <mikael.tallbo@webexpress.se>
2022-03-17 20:54:58 +0100

    validation added

--- src/components/AddHamster.jsx
+++ src/components/AddHamster.jsx
@@ -15,8 +15,72 @@ const AddHamster = () => {
     const [defeats, setDefeats ] = useState('')
     const [games, setGames ] = useState('')
     const [hamsterItems, setHamsterItems] = useState([])
-    
+    const [nameTouched, setNameTouched] = useState(false)
+    const [ageTouched, setAgeTouched] = useState(false)
+    const [favFoodTouched, setfavFoodTouched] = useState(false)
+    const [lovesTouched, setLovesTouched] = useState(false)
+    const [imgNameTouched, setImgNameTouched] = useState(false)
+
+        //Valid name
+        let nameIsValid = true;
+        let nameErrorMessage = '';
+        if( name === ''){
+            nameIsValid = false;
+            nameErrorMessage = 'Please add hamster name!'
+        }
+        let nameClass = ''
+        if(nameTouched) {
+            nameClass = (nameIsValid ? 'valid ' : 'error')
+        } 
+        //Valid age
+        let ageIsValid = true;
+        let ageErrorMessage = ''
+        if( age === ''){
+            ageIsValid = false;
+            ageErrorMessage = 'Please add hamster name!'
+        }
+        let ageClass = ''
+        if(ageTouched) {
+            ageClass = (ageIsValid ? 'valid ' : 'error')
+        } 
+        
+        //Valid favFood
+        let favFoodIsValid = true;
+        let favFoodErrorMessage = ''
+        if( favFood === ''){
+            favFoodIsValid = false;
+            favFoodErrorMessage = 'Please add hamster name!'
+        }
+        let favFoodClass = ''
+        if(favFoodTouched) {
+            favFoodClass = (favFoodIsValid ? 'valid ' : 'error')
+        } 
+        //Valid Loves
+        let lovesIsValid = true;
+        let lovesErrorMessage = ''
+        if( loves === ''){
+            lovesIsValid = false;
+            lovesErrorMessage = 'Please add hamster name!'
+        }
+        let lovesClass = ''
+        if(lovesTouched) {
+            lovesClass = (lovesIsValid ? 'valid ' : 'error')
+        } 
+        //valid imgName 
+        let imgNameIsValid = true;
+        let imgNameErrorMessage = ''
+        if( imgName === ''){
+            imgNameIsValid = false;
+            imgNameErrorMessage = 'Please add hamster name!'
+        }
+        let imgNameClass = ''
+        if(imgNameTouched) {
+            imgNameClass = (lovesIsValid ? 'valid ' : 'error')
+        } 
+        
+
         async function handleAddHamster(){
+
 
             const newHamster = {
             name: name,
@@ -30,7 +94,7 @@ const AddHamster = () => {
 
            }
            console.log(newHamster, 'newHamster')
-   
+    
         const response = await fetch('/hamsters ', {method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(newHamster) 
@@ -47,42 +111,67 @@ const AddHamster = () => {
             setWins('')
             setDefeats('')
             setGames('')
+        }
+
+   
         
-        }       
+
+
+ 
+
+ 
+            
     }
 
   return(
         <div className="add-todo-wrapper">
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form onSubmit={(e) => e.preventDefault()} className="form-box">
             <label>name
                 <input type="text" 
-                name="id"  
+                name="id"
                 value={name}
+                onBlur={()=> setNameTouched(true)}
+                className={nameClass}
                 onChange={e => setname(e.target.value)} />
+                 {nameTouched ? <div className="message">{ageErrorMessage}</div> : null}
             </label>
                 <label>age
-                <input type="text" 
+                <input type="text"  
                 name="id" 
                 value={age}
+                onBlur={()=> setAgeTouched(true)}
+                className={ageClass}
                 onChange={e => setage(e.target.value)} />
+                {ageTouched ? <div className="message">{ageErrorMessage}</div> : null}
             </label>
             <label>favFood
                 <input type="text" 
                 name="id" 
                 value={favFood}
+                onBlur={()=> setfavFoodTouched(true)}
+                className={favFoodClass}
                 onChange={e => setfavFood(e.target.value)} />
+                {favFoodTouched ? <div className="message">{nameErrorMessage}</div> : null}
             </label>
             <label>Loves
                 <input type="text" 
                 name="id" 
                 value={loves}
+                onBlur={()=> setLovesTouched(true)}
+                className={lovesClass}
                 onChange={e => setLoves(e.target.value)} />
+                {lovesTouched ? <div className="message">{lovesErrorMessage}</div> : null}
+
             </label>
             <label>Image
                 <input type="text" 
                 name="id" 
                 value={imgName}
+                onBlur={()=> setImgNameTouched(true)}
+                className={imgNameClass}
                 onChange={e => setImgName(e.target.value)} />
+                {imgNameTouched ? <div className="message">{imgNameErrorMessage}</div> : null}
+
             </label>
             </form>
             <button onClick={() => handleAddHamster()}>ADD</button>
--- src/components/Hamster.jsx
+++ src/components/Hamster.jsx
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 const Hamster = ({name, age, favFood, hamster}) => {
  
    const [hamsterDeleted, setHamsterDeleted] = useState(false)
-   const [IsShowing, setIsShowing] = useState(false)
 
 
     async function deleteHamster(id) {
@@ -10,23 +9,13 @@ const Hamster = ({name, age, favFood, hamster}) => {
         setHamsterDeleted(true)
     
         }
-
-        function handleMouseEnter(e){
-            if( setIsShowing(e.target)){
-                return IsShowing
-            }
-           
-            console.log(IsShowing , 'hello')
-       
-
-        }
    
          return (
                hamsterDeleted ? null : (  
                       <div className="hamster-info-container">
                 <button onClick={() => deleteHamster(hamster.id)}>Delete</button>
                 <div className="hamster-info-text" 
-                 onMouseEnter={handleMouseEnter}> 
+                 > 
                     <h2>{hamster.name}</h2> 
                     <p>Ålder:{hamster.age}</p> 
                     <p>Favorit mat:{hamster.favFood}</p> 
